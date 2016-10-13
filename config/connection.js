45583df2167da16610ecbb5f710b0895dd5df7a8
C Fu <C Fu>
2016-10-13 07:10:47 -0700

    fixing image and view issue

--- config/connection.js
+++ config/connection.js
@@ -15,6 +15,8 @@ module.exports = function()  {
 
       connection.connect(function(err) {
         if (err) throw err;
+
+        console.log('connected as id ' + connection.threadId);
       });
 
       return connection;
